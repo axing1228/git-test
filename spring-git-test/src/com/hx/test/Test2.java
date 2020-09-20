fe7b0fb251ef96ddeafba35bdcce14479d5d7f83
zhengzheng <zhenghuaxing@163.com>
2020-09-20 10:17:25 +0800

    增加Test222222222

--- spring-git-test/src/com/hx/test/Test2.java
+++ spring-git-test/src/com/hx/test/Test2.java
@@ -3,5 +3,7 @@ package com.hx.test;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("test2");
+        System.out.println("test2");
     }
 }
